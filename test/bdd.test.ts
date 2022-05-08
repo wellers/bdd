65d5a81b76d34b878d211c23c01ebfb435ec98da
wellers <paul_welbourne@yahoo.co.uk>
2022-05-08 23:53:29 +0100

    Add test for then().

--- test/bdd.test.ts
+++ test/bdd.test.ts
@@ -6,7 +6,12 @@ new BddTest()
 	error: 'given() must be called with a message before should().'
 }))
 .should('throw error', ({ error }) => strictEqual(error, 'given() must be called with a message before should().'))
-.then((test: any) => {
-	clearTimeout(test.x);
-})
+.run();
+
+new BddTest()
+.given('message is passed', () => ({
+	error: 'both given() and should() must be called with a message before then().'
+}))
+.should('', ({ error }) => strictEqual(error, 'both given() and should() must be called with a message before then().'))
+.then(() => {})
 .run();
